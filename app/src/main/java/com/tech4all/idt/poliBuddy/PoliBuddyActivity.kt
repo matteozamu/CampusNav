b2c3121d3c0477b8edc9c053faa8284cff0acc97
Diego41ITA <diego.quattrone@mail.polimi.it>
2025-05-19 16:35:27 +0200

    Refactor: Merge PlanEventActivity into PoliBuddyActivity
    The functionality of `PlanEventActivity` has been integrated into `PoliBuddyActivity`.
    `PlanEventActivity.kt` and `activity_plan_event.xml` have been deleted.
    
    Changes in `PoliBuddyActivity`:
    - Tabs are now "Help Request" and "Events".
    - The "Events" tab now inflates `layout_helper.xml` (previously `activity_plan_event.xml`), which includes the "Create new event" button, "Available Events" title, refresh button, and RecyclerView for events.
    - Logic for creating new events, refreshing events, and displaying them in the RecyclerView is now handled within `PoliBuddyActivity` when the "Events" tab is selected.
    - The "Plan new event" button has been removed from the main layout of `activity_polibuddy.xml`.
    
    Changes in `layout_helped.xml`:
    - The content previously in `layout_helper.xml` (selecting a person to help) has been moved to the bottom of `layout_helped.xml`.
    
    `PlanEventActivity` has been removed from `AndroidManifest.xml`.

--- app/src/main/java/com/tech4all/idt/poliBuddy/PoliBuddyActivity.kt
+++ app/src/main/java/com/tech4all/idt/poliBuddy/PoliBuddyActivity.kt
@@ -3,10 +3,9 @@ package com.tech4all.idt.poliBuddy
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.ImageButton
+import android.widget.*
 import androidx.activity.addCallback
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -17,33 +16,26 @@ import com.google.android.material.tabs.TabLayout
 import com.tech4all.idt.R
 import com.tech4all.idt.SupabaseHelper
 import kotlinx.coroutines.launch
-import android.view.LayoutInflater
 
+class PoliBuddyActivity : AppCompatActivity() {
 
-class PoliBuddyActivity : AppCompatActivity () {
-    private lateinit var planNewEventButton: Button;
+    private lateinit var tabLayout: TabLayout
+    private lateinit var dynamicContainer: FrameLayout
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_polibuddy)
 
-        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-        val dynamicContainer = findViewById<FrameLayout>(R.id.dynamicContainer)
+        tabLayout = findViewById(R.id.tabLayout)
+        dynamicContainer = findViewById(R.id.dynamicContainer)
 
-        planNewEventButton = findViewById(R.id.planNewEventButton)
-        planNewEventButton.setOnClickListener {
-            val intent = Intent(this, PlanEventActivity::class.java)
-            startActivity(intent)
-        }
-
-        tabLayout.addTab(tabLayout.newTab().setText("Send an Help Request"))
-        tabLayout.addTab(tabLayout.newTab().setText("Answer to an Help Request"))
+        tabLayout.addTab(tabLayout.newTab().setText("Help Request"))
+        tabLayout.addTab(tabLayout.newTab().setText("Events"))
 
         // Show the "Send an Help Request" layout initially
         inflateDynamicLayout(R.layout.layout_helped)
 
-        // Handle tab selection
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 when (tab.position) {
@@ -56,25 +48,57 @@ class PoliBuddyActivity : AppCompatActivity () {
             override fun onTabReselected(tab: TabLayout.Tab?) {}
         })
 
-        // Set up the action bar with a back button
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        // Action bar back button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         onBackPressedDispatcher.addCallback(this) {
             finish()
         }
-
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun inflateDynamicLayout(layoutId: Int) {
-        val container = findViewById<FrameLayout>(R.id.dynamicContainer)
-        container.removeAllViews()
-        val view = LayoutInflater.from(this).inflate(layoutId, container, false)
-        container.addView(view)
+        dynamicContainer.removeAllViews()
+        val view = LayoutInflater.from(this).inflate(layoutId, dynamicContainer, false)
+        dynamicContainer.addView(view)
+
+        when (layoutId) {
+            R.layout.layout_helped -> {
+                val helpButton = view.findViewById<LinearLayout>(R.id.help_button)
+                helpButton.setOnClickListener {
+                    // Gestisci richiesta di aiuto
+                    Toast.makeText(this, "Help request sent", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            R.layout.layout_helper -> {
+                val createNewEventButton = view.findViewById<Button>(R.id.createNewEventButton)
+                val refreshButton = view.findViewById<ImageButton>(R.id.refreshButton)
+                val recyclerView = view.findViewById<RecyclerView>(R.id.eventRecyclerView)
+
+                createNewEventButton.setOnClickListener {
+                    val intent = Intent(this, CreateEventActivity::class.java)
+                    startActivity(intent)
+                }
+
+                refreshButton.setOnClickListener {
+                    refreshEvents(recyclerView)
+                }
+
+                recyclerView.layoutManager = LinearLayoutManager(this)
+                refreshEvents(recyclerView)
+            }
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun refreshEvents(recyclerView: RecyclerView) {
+        lifecycleScope.launch {
+            val events = SupabaseHelper.getUpcomingEvents()
+            val adapter = EventAdapter(events, this@PoliBuddyActivity)
+            recyclerView.adapter = adapter
+        }
+    }
 
-    /**
-     * Handle the back button press in the action bar.
-     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
@@ -85,4 +109,3 @@ class PoliBuddyActivity : AppCompatActivity () {
         }
     }
 }
-
